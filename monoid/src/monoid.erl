7be51230503f30303e8f99f4ab1e2b03ff532bac
josh chapman <cha23071@byui.edu>
2025-10-22 14:42:11 -0600

    working monoid

--- monoid/src/monoid.erl
+++ monoid/src/monoid.erl
@@ -1,3 +1,8 @@
 -module(monoid).
+-author(josh).
 
--export([]).
+-export([aandm_monoid/2]).
+
+aandm_monoid(Element1,Element2) -> 
+    ({lists:foldl(fun(X,Acc) -> X + Acc end, 0, Element1 ++ Element2),
+    lists:foldl(fun(X,Acc) -> X * Acc end, 1, Element1 ++ Element2)}).
