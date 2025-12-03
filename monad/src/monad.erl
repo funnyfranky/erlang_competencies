466bcdd305b3e6a9a3dc06e9f56ba1908413800a
josh <cha23071@byui.edu>
2025-12-03 15:46:08 -0700

    monads update

--- monad/src/monad.erl
+++ monad/src/monad.erl
@@ -29,8 +29,8 @@ sub5(X) ->
     {X - 5, ["subtracted 5"]}.
 
 test() ->
-    Start = fun_unit(4),
-    Step1 = fun_bind(Start, fun add2/1),  %% {6,  ["added 2"]}
-    Step2 = fun_bind(Step1, fun mul3/1),  %% {18, ["added 2","multiplied by 3"]}
-    Result = fun_bind(Step2, fun sub5/1), %% {13, ["added 2","multiplied by 3","subtracted 5"]}
+    One = fun_unit(4),
+    Two = fun_bind(One, fun add2/1),  %% {6,  ["added 2"]}
+    Three = fun_bind(Two, fun mul3/1),  %% {18, ["added 2","multiplied by 3"]}
+    Result = fun_bind(Three, fun sub5/1), %% {13, ["added 2","multiplied by 3","subtracted 5"]}
     Result.
